bbcc20f6b487d69f05cc1b9c50f540dd983e598f
vvmafra <victorvmafra97@gmail.com>
2023-01-17 04:03:36 -0300

    versão final com responsividade e fim do jogo

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,12 @@
 let primeiroCard, segundoCard;
 let bloquearCard = false;
+let cardsAbertas = 0;
+let jogadas = 0;
+let lol = 0;
+
 
 function clicar(cardClicado){
-    if(bloquearCard ===true) return false;
+    if(bloquearCard === true) return false;
     cardClicado.classList.add("clicado");
     
     
@@ -14,15 +18,17 @@ function clicar(cardClicado){
     segundoCard = cardClicado;
 
     check();
-    
+
+    if(cardsAbertas === qtdCards) {
+      setTimeout (() => {alert('Você ganhou em ' + jogadas + ' jogadas!');
+    }, 300)
+    }
     
 } 
 
 let qtdCards = 0;
 
 qtdCards = prompt("Deseja jogar com quantas cartas?");
-
-console.log(qtdCards);
 
 while (qtdCards % 2 !== 0 ||  qtdCards < 4 || qtdCards > 14) {
     qtdCards = prompt("Para inicializar o jogo, favor colocar quantidade de cartas par entre 4 e 14:")
@@ -55,17 +61,18 @@ const arrayCards = [
         const cards = document.querySelector('.cards');
         cards.innerHTML = '';
         arrayCards.length = qtdCards;
+        qtdCards = Number(qtdCards);
         arrayCards.sort(comparador);
 
         
         for (let indice=0; indice < arrayCards.length; indice++){
             let template = `
-              <div class="card" onclick="clicar(this)" data-card="${arrayCards[indice]}">
+              <div class="card" onclick="clicar(this)" data-test="card" data-card="${arrayCards[indice]}">
                 <div class="back-face face">
-                 <img src="${arrayCards[indice]}" class="tras">
+                 <img src="${arrayCards[indice]}" class="tras" data-test="face-up-image">
                 </div> <!--back-face-->  
                 <div class="front-face face"  >
-                  <img src="imagens/back.png" class="frente">
+                  <img src="imagens/back.png" class="frente" data-test="face-down-image">
                 </div> <!--front-face-->
               </div> <!--card-->
             `;
@@ -77,10 +84,16 @@ const arrayCards = [
         criarCard();
 
         function check() {
+          jogadas = jogadas + 2;
+          console.log(jogadas);
           let checkCard = primeiroCard.dataset.card === segundoCard.dataset.card;
-          console.log(checkCard);
-
+          
+          if (checkCard === true) {
+            cardsAbertas = cardsAbertas + 2;
+          }
+          console.log(cardsAbertas);
           !checkCard ? desvirar(): resetarCards(checkCard);
+          
         }
 
         function desvirar() {
